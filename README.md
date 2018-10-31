ab7323544b960eb4518c845770942125db4162b7
Caio Moura <caiomoura1994@gmail.com>
2018-10-31 01:11:48 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # adonis-php
+* To run
+  - `docker-compose up -d`
+  - Acess `localhost:8000` to PHP project
+  - Acess `localhost:8080` to adminer dashboard
---
+++ bd.php
@@ -0,0 +1,11 @@
+<?php
+  $host = "db";
+  $usuario = "root";
+  $senha = "pass";
+  $base_dados = "rede_social";
+  $con = new mysqli($host, $usuario, $senha, $base_dados);
+
+  if ($con->connect_error) {
+    echo "Erro ao conectar<br>";
+  }
+?>
